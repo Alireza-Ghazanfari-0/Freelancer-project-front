bd8290a72679edc9b4861a955233e5944504f512
Alireza <alireza.ghazanfari@yahoo.com>
2025-07-09 23:37:32 +0330

    Update Auth.jsx

--- src/features/authentication/Auth.jsx
+++ src/features/authentication/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import SendOTP from "./SendOTP";
-import CheckOTP from "./CheckOtp";
+import CheckOTP from "./CheckOTP";
 import DarkToggle from "../../ui/DarkToggle";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
